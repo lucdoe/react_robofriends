d71806337e78849a90a58faeb7ece99245d24e71
lucdoe <doerrstein.lucca@yahoo.de>
2020-05-17 14:24:51 +0200

    Updates

---
+++ precache-manifest.49b2a4ff8d7a9f024bc3a794f80ab3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafb9afece543aae46d65c238dc0721d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ca58f7d96c7a68daae10",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "13cb1f50db8c57334ef2",
+    "url": "/robofriends/static/css/main.0ef1d88c.chunk.css"
+  },
+  {
+    "revision": "ca58f7d96c7a68daae10",
+    "url": "/robofriends/static/js/2.86e96b72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.86e96b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cb1f50db8c57334ef2",
+    "url": "/robofriends/static/js/main.fa3b6729.chunk.js"
+  },
+  {
+    "revision": "43527eccbe96a1e03fa7",
+    "url": "/robofriends/static/js/runtime-main.20450fba.js"
+  }
+]);
